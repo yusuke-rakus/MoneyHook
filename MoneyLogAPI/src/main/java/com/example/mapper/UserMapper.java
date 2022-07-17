5ee3713f3a9dad50b14ac1e370d03b97827ff54f
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-07-17 21:58:28 +0900

    ログイン処理を実装

--- MoneyLogAPI/src/main/java/com/example/mapper/UserMapper.java
+++ MoneyLogAPI/src/main/java/com/example/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.example.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.domain.User;
+import com.example.form.LoginForm;
 import com.example.form.RegistUserForm;
 
 @Mapper
@@ -9,5 +11,8 @@ public interface UserMapper {
 
 	/** ユーザー登録 */
 	public void registUser(RegistUserForm form);
+
+	/** ログイン */
+	public User login(LoginForm form);
 
 }
--- MoneyLogAPI/src/main/java/com/example/service/UserService.java
+++ MoneyLogAPI/src/main/java/com/example/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.common.Message;
 import com.example.common.Status;
 import com.example.domain.User;
+import com.example.form.LoginForm;
 import com.example.form.RegistUserForm;
 import com.example.mapper.UserMapper;
+import com.example.response.LoginResponse;
 import com.example.response.RegistUserResponse;
 
 @Service
@@ -39,6 +42,35 @@ public class UserService {
 			res.setStatus(Status.ERROR.getStatus());
 			res.setMessage(Message.MAIL_ADDRESS_ALREADY_REGISTERED.getMessage());
 		}
+		return res;
+	}
+
+	/** ログイン */
+	public LoginResponse login(LoginForm form) {
+		LoginResponse res = new LoginResponse();
+		String inputPassword = form.getPassword();
+
+		try {
+			User user = userMapper.login(form);
+
+			if (Objects.isNull(user)) {
+				throw new NullPointerException();
+			} else if (!user.getPassword().equals(inputPassword)) {
+				throw new Exception();
+			}
+			user.setPassword(null);
+			res.setUser(user);
+
+		} catch (NullPointerException e) {
+			res.setStatus(Status.ERROR.getStatus());
+			// メールアドレスをもとにユーザー情報が取得できなかった場合のエラー
+			res.setMessage(Message.UNREGISTERED_MAIL_ADDRESS.getMessage());
+		} catch (Exception e) {
+			res.setStatus(Status.ERROR.getStatus());
+			// パスワードが不一致の場合のエラー
+			res.setMessage(Message.PASSWORD_INCORRECT.getMessage());
+		}
+		
 		return res;
 	}
 

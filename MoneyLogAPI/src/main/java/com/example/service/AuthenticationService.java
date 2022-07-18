317d251c333217df171579cb8c99260f61d48fd6
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-07-18 13:06:35 +0900

    ユーザー認証統一化の修正

--- MoneyLogAPI/src/main/java/com/example/service/AuthenticationService.java
+++ MoneyLogAPI/src/main/java/com/example/service/AuthenticationService.java
@@ -1,12 +1,16 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.common.AuthenticationException;
 import com.example.common.Message;
 import com.example.form.form;
 import com.example.mapper.UserMapper;
 
+@Service
+@Transactional
 public class AuthenticationService {
 
 	@Autowired

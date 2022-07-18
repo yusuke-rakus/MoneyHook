3761bef4c4b6f8aa1f4f6597b5ad6298ba06097e
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-07-18 21:09:35 +0900

    ユーザー情報の取得

--- MoneyLogAPI/src/main/java/com/example/controller/UserController.java
+++ MoneyLogAPI/src/main/java/com/example/controller/UserController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.form.GetUserInfoForm;
 import com.example.form.LoginForm;
 import com.example.form.RegistUserForm;
+import com.example.response.GetUserInfoResponse;
 import com.example.response.LoginResponse;
 import com.example.response.RegistUserResponse;
 import com.example.service.UserService;
@@ -29,6 +31,12 @@ public class UserController {
 	@PostMapping("/login")
 	public LoginResponse login(@RequestBody LoginForm form) {
 		return userService.login(form);
+	}
+	
+	/** ユーザー情報の取得 */
+	@PostMapping("/getUserInfo")
+	public GetUserInfoResponse getUserInfo(@RequestBody GetUserInfoForm form) {
+		return userService.getUserInfo(form);
 	}
 
 }
--- MoneyLogAPI/src/main/java/com/example/service/UserService.java
+++ MoneyLogAPI/src/main/java/com/example/service/UserService.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.common.AuthenticationException;
 import com.example.common.Message;
 import com.example.common.Status;
 import com.example.domain.User;
+import com.example.form.GetUserInfoForm;
 import com.example.form.LoginForm;
 import com.example.form.RegistUserForm;
 import com.example.mapper.UserMapper;
+import com.example.response.GetUserInfoResponse;
 import com.example.response.LoginResponse;
 import com.example.response.RegistUserResponse;
 
@@ -22,6 +25,9 @@ public class UserService {
 
 	@Autowired
 	private UserMapper userMapper;
+
+	@Autowired
+	private AuthenticationService authenticationService;
 
 	/** ユーザー登録 */
 	public RegistUserResponse registUser(RegistUserForm form) {
@@ -70,7 +76,25 @@ public class UserService {
 			// パスワードが不一致の場合のエラー
 			res.setMessage(Message.PASSWORD_INCORRECT.getMessage());
 		}
-		
+
+		return res;
+	}
+
+	/** ユーザー情報の取得 */
+	public GetUserInfoResponse getUserInfo(GetUserInfoForm form) {
+		GetUserInfoResponse res = new GetUserInfoResponse();
+
+		try {
+			User user = userMapper.getUserInfo(form);
+			if (Objects.isNull(user)) {
+				throw new Exception();
+			}
+			res.setUserInfo(user);
+		} catch (Exception e) {
+			res.setStatus(Status.ERROR.getStatus());
+			res.setMessage(Message.USER_INFO_GET_FAILED.getMessage());
+		}
+
 		return res;
 	}
 

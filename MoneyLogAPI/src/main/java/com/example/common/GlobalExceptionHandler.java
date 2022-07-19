ed340c658a9bd8dc0861c6cfca680089af2dd745
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2022-07-19 11:15:28 +0900

    JavaDocを追加。

--- MoneyLogAPI/src/main/java/com/example/common/GlobalExceptionHandler.java
+++ MoneyLogAPI/src/main/java/com/example/common/GlobalExceptionHandler.java
@@ -7,9 +7,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.common.exception.AuthenticationException;
 import com.example.response.ExceptionResponse;
 
+/**
+ * 本アプリケーションの例外を処理するクラスです。
+ * 例外発生時はレスポンスを返します。
+ * 
+ * @author cyjoh
+ *
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler  {
 
+    /**
+     * 定義されていない例外を処理します。
+     * 
+     * @return 失敗結果
+     */
     @ExceptionHandler( Exception.class )
 	@ResponseBody
     public ExceptionResponse handleSystemExceptionError() {
@@ -19,6 +31,11 @@ public class GlobalExceptionHandler  {
         return res;
     }
 
+    /**
+     * ユーザー認証エラーを処理します。
+     * 
+     * @return 失敗結果
+     */
     @ExceptionHandler( AuthenticationException.class )
     @ResponseBody
     public ExceptionResponse handleAuthenticationError() {
--- MoneyLogAPI/src/main/java/com/example/common/exception/SystemException.java
+++ MoneyLogAPI/src/main/java/com/example/common/exception/SystemException.java
@@ -1,5 +1,12 @@
 package com.example.common.exception;
 
+/**
+ * アプリケーション全体におけるシステム例外の基底クラスです。
+ * システム例外を実装する際はこのクラスを継承してください。
+ * 
+ * @author cyjoh
+ *
+ */
 public class SystemException extends Exception {
 	
 	private static final long serialVersionUID = 1L;

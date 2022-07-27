fd27831c467d53d108e0802020227a9bae90b896
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-07-27 21:18:58 +0900

    テーマカラーの変更

--- MoneyLogAPI/src/main/java/com/example/mapper/UserMapper.java
+++ MoneyLogAPI/src/main/java/com/example/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import com.example.domain.User;
 import com.example.form.ChangeEmailForm;
 import com.example.form.ChangePasswordForm;
+import com.example.form.EditThemeColorForm;
 import com.example.form.GetUserInfoForm;
 import com.example.form.LoginForm;
 import com.example.form.RegistUserForm;
@@ -29,5 +30,8 @@ public interface UserMapper {
 	
 	/** メールアドレスを変更 */
 	public boolean changeEmail(ChangeEmailForm form);
+	
+	/** テーマカラーの変更 */
+	public boolean editThemeColor(EditThemeColorForm form);
 
 }

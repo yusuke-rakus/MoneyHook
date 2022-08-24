8ca274136a91770bdcee2e5b8bae808722e17dff
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-08-24 22:58:06 +0900

    したつもり貯金総額画面 グラフの追加

--- moneylog-ui/src/page/TotalSaving.jsx
+++ moneylog-ui/src/page/TotalSaving.jsx
@@ -4,14 +4,29 @@ import UncategorizedSavingCard from "../components/UncategorizedSavingCard";
 import "./page_CSS/TotalSaving.css";
 import "./page_CSS/common.css";
 import AddSharpIcon from "@mui/icons-material/AddSharp";
+import { Line } from "react-chartjs-2";
+
+const data = {
+  labels: ["2月", "3月", "4月", "5月", "6月", "7月"],
+  datasets: [
+    {
+      label: "累計額",
+      borderColor: "#03A9F4",
+      data: [300, 1000, 2100, 3100, 3400, 4000, 4800],
+    },
+  ],
+};
 
 const TotalSaving = () => {
   return (
     <>
+      <div className="LineChartArea">
+        <Line data={data} />
+      </div>
       <div className="SavingTargetCardArea">
         <SavingTargetCard />
         <SavingTargetCard />
-        <AddSharpIcon className="AddSavingTargetButton" />
+        <AddSharpIcon fontSize="large" className="AddSavingTargetButton" />
       </div>
 
       <UncategorizedSavingCard className="UncategorizedSavingCard" />

d944413e51aba3c6ec46583ec3b59827cd9984df
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-08-28 01:06:14 +0900

    貯金総額画面のグラフデザインをアップデート

--- moneylog-ui/src/page/TotalSaving.jsx
+++ moneylog-ui/src/page/TotalSaving.jsx
@@ -6,15 +6,49 @@ import "./page_CSS/common.css";
 import AddSharpIcon from "@mui/icons-material/AddSharp";
 import { Line } from "react-chartjs-2";
 
+/** グラフデータ */
 const data = {
   labels: ["2月", "3月", "4月", "5月", "6月", "7月"],
   datasets: [
     {
       label: "累計額",
       borderColor: "#03A9F4",
+      pointBorderColor: "#03A9F4",
+      pointBackgroundColor: "#03A9F4",
       data: [300, 1000, 2100, 3100, 3400, 4000, 4800],
     },
   ],
+};
+
+/** グラフオプション */
+const option = {
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        font: {
+          size: 18,
+        },
+      },
+      grid: {
+        display: false,
+        drawBorder: false,
+      },
+    },
+    y: {
+      ticks: {
+        display: false,
+      },
+      grid: {
+        display: false,
+        drawBorder: false,
+      },
+    },
+  },
 };
 
 const TotalSaving = () => {
@@ -25,7 +59,7 @@ const TotalSaving = () => {
         <span>nnn,nnn</span>
       </div>
       <div className="LineChartArea">
-        <Line data={data} />
+        <Line data={data} options={option} />
       </div>
       <div className="SavingTargetCardArea">
         <SavingTargetCard />

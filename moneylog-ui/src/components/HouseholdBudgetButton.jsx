4e48ebe9f3d893ddcfe77345c80c3b7792129ca6
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-09-04 09:49:06 +0900

    取引追加ボタンのアップデート

--- moneylog-ui/src/components/HouseholdBudgetButton.jsx
+++ moneylog-ui/src/components/HouseholdBudgetButton.jsx
@@ -1,38 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
-import { CSSTransition } from "react-transition-group";
 import "./components_CSS/HouseholdBudgeButton.css";
 
 const HouseholdBudgetButton = (props) => {
-  // 変数を宣言
-  const { BoxText } = props;
-  const [BoxStatus, setBoxStatus] = useState(true);
+  const { setModalWindow, buttonText } = props;
 
   return (
-    <>
-      <div className="add-house-hold-budget-button">
-        <CSSTransition
-          in={BoxStatus}
-          timeout={500}
-          unmountOnExit
-          classNames="HouseholdBudgetButton-show"
-        >
-          <div className="HouseholdBudgetButton">
-            <p className="box-text-style">{BoxText}</p>
-          </div>
-        </CSSTransition>
-        <Fab
-          onMouseEnter={() => setBoxStatus(true)}
-          onMouseLeave={() => setBoxStatus(false)}
-          className="button-style"
-          color="info"
-          aria-label="add"
-        >
+    <div className="householdBudgetButton" onClick={() => setModalWindow(true)}>
+      <div className="textArea">
+        <p className="textStyle">{buttonText}</p>
+      </div>
+
+      <div className="buttonArea">
+        <Fab color="info" aria-label="add" sx={{ boxShadow: "0" }}>
           <AddIcon />
         </Fab>
       </div>
-    </>
+    </div>
   );
 };
 

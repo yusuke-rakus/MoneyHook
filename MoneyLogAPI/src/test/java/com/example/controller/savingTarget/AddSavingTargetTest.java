f4fd340130f51692a30089b99cf726c706ae45fe
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2023-06-25 22:29:53 +0900

    貯金目標のテスト追加

--- MoneyLogAPI/src/test/java/com/example/controller/savingTarget/AddSavingTargetTest.java
+++ MoneyLogAPI/src/test/java/com/example/controller/savingTarget/AddSavingTargetTest.java
@@ -1,7 +1,9 @@
 package com.example.controller.savingTarget;
 
 import com.example.common.Status;
+import com.example.common.message.ErrorMessage;
 import com.example.common.message.SuccessMessage;
+import com.example.common.message.ValidatingMessage;
 import com.example.domain.SavingTarget;
 import com.example.form.AddSavingTargetForm;
 import com.example.form.GetSavingTargetListForm;
@@ -29,47 +31,145 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AddSavingTargetTest {
 
-    final String URL = "/savingTarget/addSavingTarget";
-    final String USER_ID = "a77a6e94-6aa2-47ea-87dd-129f580fb669";
+	final String URL = "/savingTarget/addSavingTarget";
+	final String USER_ID = "a77a6e94-6aa2-47ea-87dd-129f580fb669";
+	final String FAIL_USER_ID = "fail_user_id";
+	final String NULL_USER_ID = null;
 
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper mapper;
+	@Autowired
+	private ObjectMapper mapper;
 
-    @Autowired
-    private SavingTargetMapper savingTargetMapper;
+	@Autowired
+	private SavingTargetMapper savingTargetMapper;
 
-    @Test
-    @Transactional(readOnly = false)
-    void addSavingTargetTest() throws Exception {
+	@Test
+	@Transactional(readOnly = false)
+	void addSavingTargetTest() throws Exception {
 
-        String savingTargetName = "貯金目標追加テスト";
-        BigInteger targetAmount = BigInteger.valueOf(1000);
+		String savingTargetName = "貯金目標追加テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
 
-        AddSavingTargetForm req = new AddSavingTargetForm();
-        req.setUserId(USER_ID);
-        req.setSavingTargetName(savingTargetName);
-        req.setTargetAmount(targetAmount);
+		AddSavingTargetForm req = new AddSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
 
-        String result = mvc
-                .perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
-                .andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
-                .getContentAsString(Charset.defaultCharset());
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
 
-        AddSavingTargetResponse response = mapper.readValue(result, AddSavingTargetResponse.class);
+		AddSavingTargetResponse response = mapper.readValue(result, AddSavingTargetResponse.class);
 
-        /* 検証 */
-        assertEquals(Status.SUCCESS.getStatus(), response.getStatus());
-        assertEquals(SuccessMessage.SAVING_TARGET_INSERT_SUCCESSED, response.getMessage());
+		/* 検証 */
+		assertEquals(Status.SUCCESS.getStatus(), response.getStatus());
+		assertEquals(SuccessMessage.SAVING_TARGET_INSERT_SUCCESSED, response.getMessage());
 
-        GetSavingTargetListForm form = new GetSavingTargetListForm();
-        form.setUserNo(2L);
-        SavingTarget savingTarget =
-                savingTargetMapper.getSavingTargetList(form).stream().filter(i -> savingTargetName.equals(i.getSavingTargetName()))
-                        .collect(Collectors.toList()).get(0);
+		GetSavingTargetListForm form = new GetSavingTargetListForm();
+		form.setUserNo(2L);
+		SavingTarget savingTarget =
+				savingTargetMapper.getSavingTargetList(form).stream().filter(i -> savingTargetName.equals(i.getSavingTargetName()))
+						.collect(Collectors.toList()).get(0);
 
-        assertEquals(targetAmount, savingTarget.getTargetAmount());
-    }
+		assertEquals(targetAmount, savingTarget.getTargetAmount());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void addSavingTargetUserError01Test() throws Exception {
+
+		String savingTargetName = "貯金目標追加テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		AddSavingTargetForm req = new AddSavingTargetForm();
+		req.setUserId(FAIL_USER_ID);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		AddSavingTargetResponse response = mapper.readValue(result, AddSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ErrorMessage.AUTHENTICATION_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void addSavingTargetUserError02Test() throws Exception {
+
+		String savingTargetName = "貯金目標追加テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		AddSavingTargetForm req = new AddSavingTargetForm();
+		req.setUserId(NULL_USER_ID);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		AddSavingTargetResponse response = mapper.readValue(result, AddSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ErrorMessage.AUTHENTICATION_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void addSavingTargetTargetNameError01Test() throws Exception {
+
+		String savingTargetName = "";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		AddSavingTargetForm req = new AddSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		AddSavingTargetResponse response = mapper.readValue(result, AddSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ValidatingMessage.BOTH_OF_ID_AND_NAME_EMPTY_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void addSavingTargetTargetAmountError01Test() throws Exception {
+
+		String savingTargetName = "貯金目標追加テスト";
+		BigInteger targetAmount = null;
+
+		AddSavingTargetForm req = new AddSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		AddSavingTargetResponse response = mapper.readValue(result, AddSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ValidatingMessage.SAVING_TARGET_AMOUNT_EMPTY_ERROR, response.getMessage());
+	}
 }
--- MoneyLogAPI/src/test/java/com/example/controller/savingTarget/EditSavingTargetTest.java
+++ MoneyLogAPI/src/test/java/com/example/controller/savingTarget/EditSavingTargetTest.java
@@ -1,7 +1,9 @@
 package com.example.controller.savingTarget;
 
 import com.example.common.Status;
+import com.example.common.message.ErrorMessage;
 import com.example.common.message.SuccessMessage;
+import com.example.common.message.ValidatingMessage;
 import com.example.domain.SavingTarget;
 import com.example.form.EditSavingTargetForm;
 import com.example.form.GetSavingTargetListForm;
@@ -29,50 +31,182 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class EditSavingTargetTest {
 
-    final String URL = "/savingTarget/editSavingTarget";
-    final String USER_ID = "a77a6e94-6aa2-47ea-87dd-129f580fb669";
+	final String URL = "/savingTarget/editSavingTarget";
+	final String USER_ID = "a77a6e94-6aa2-47ea-87dd-129f580fb669";
+	final String FAIL_USER_ID = "fail_user_id";
+	final String NULL_USER_ID = null;
 
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-    @Autowired
-    private ObjectMapper mapper;
+	@Autowired
+	private ObjectMapper mapper;
 
-    @Autowired
-    private SavingTargetMapper savingTargetMapper;
+	@Autowired
+	private SavingTargetMapper savingTargetMapper;
 
-    @Test
-    @Transactional(readOnly = false)
-    void editSavingTargetTest() throws Exception {
+	@Test
+	@Transactional(readOnly = false)
+	void editSavingTargetTest() throws Exception {
 
-        Long savingTargetId = 1L;
-        String savingTargetName = "貯金目標編集テスト";
-        BigInteger targetAmount = BigInteger.valueOf(1000);
+		Long savingTargetId = 1L;
+		String savingTargetName = "貯金目標編集テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
 
-        EditSavingTargetForm req = new EditSavingTargetForm();
-        req.setUserId(USER_ID);
-        req.setSavingTargetId(savingTargetId);
-        req.setSavingTargetName(savingTargetName);
-        req.setTargetAmount(targetAmount);
+		EditSavingTargetForm req = new EditSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetId(savingTargetId);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
 
-        String result = mvc
-                .perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
-                .andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
-                .getContentAsString(Charset.defaultCharset());
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
 
-        EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
+		EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
 
-        /* 検証 */
-        assertEquals(Status.SUCCESS.getStatus(), response.getStatus());
-        assertEquals(SuccessMessage.SAVING_TARGET_EDIT_SUCCESSED, response.getMessage());
+		/* 検証 */
+		assertEquals(Status.SUCCESS.getStatus(), response.getStatus());
+		assertEquals(SuccessMessage.SAVING_TARGET_EDIT_SUCCESSED, response.getMessage());
 
-        GetSavingTargetListForm form = new GetSavingTargetListForm();
-        form.setUserNo(2L);
-        SavingTarget savingTarget =
-                savingTargetMapper.getSavingTargetList(form).stream().filter(i -> savingTargetId.equals(i.getSavingTargetId()))
-                        .collect(Collectors.toList()).get(0);
+		GetSavingTargetListForm form = new GetSavingTargetListForm();
+		form.setUserNo(2L);
+		SavingTarget savingTarget =
+				savingTargetMapper.getSavingTargetList(form).stream().filter(i -> savingTargetId.equals(i.getSavingTargetId()))
+						.collect(Collectors.toList()).get(0);
 
-        assertEquals(savingTargetName, savingTarget.getSavingTargetName());
-        assertEquals(targetAmount, savingTarget.getTargetAmount());
-    }
+		assertEquals(savingTargetName, savingTarget.getSavingTargetName());
+		assertEquals(targetAmount, savingTarget.getTargetAmount());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void editSavingTargetUserError01Test() throws Exception {
+
+		Long savingTargetId = 1L;
+		String savingTargetName = "貯金目標編集テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		EditSavingTargetForm req = new EditSavingTargetForm();
+		req.setUserId(FAIL_USER_ID);
+		req.setSavingTargetId(savingTargetId);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ErrorMessage.AUTHENTICATION_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void editSavingTargetUserError02Test() throws Exception {
+
+		Long savingTargetId = 1L;
+		String savingTargetName = "貯金目標編集テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		EditSavingTargetForm req = new EditSavingTargetForm();
+		req.setUserId(NULL_USER_ID);
+		req.setSavingTargetId(savingTargetId);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ErrorMessage.AUTHENTICATION_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void editSavingTargetTargetIdError01Test() throws Exception {
+
+		Long savingTargetId = null;
+		String savingTargetName = "貯金目標編集テスト";
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		EditSavingTargetForm req = new EditSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetId(savingTargetId);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ValidatingMessage.ID_EMPTY_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void editSavingTargetTargetNameError01Test() throws Exception {
+
+		Long savingTargetId = 1L;
+		String savingTargetName = null;
+		BigInteger targetAmount = BigInteger.valueOf(1000);
+
+		EditSavingTargetForm req = new EditSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetId(savingTargetId);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ValidatingMessage.SAVING_TARGET_NAME_EMPTY_ERROR, response.getMessage());
+	}
+
+	@Test
+	@Transactional(readOnly = false)
+	void editSavingTargetTargetAmountError01Test() throws Exception {
+
+		Long savingTargetId = 1L;
+		String savingTargetName = "貯金目標編集テスト";
+		BigInteger targetAmount = null;
+
+		EditSavingTargetForm req = new EditSavingTargetForm();
+		req.setUserId(USER_ID);
+		req.setSavingTargetId(savingTargetId);
+		req.setSavingTargetName(savingTargetName);
+		req.setTargetAmount(targetAmount);
+
+		String result = mvc
+				.perform(post(URL).content(mapper.writeValueAsString(req)).contentType(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk()).andReturn().getResponse()
+				.getContentAsString(Charset.defaultCharset());
+
+		EditSavingTargetResponse response = mapper.readValue(result, EditSavingTargetResponse.class);
+
+		/* 検証 */
+		assertEquals(Status.ERROR.getStatus(), response.getStatus());
+		assertEquals(ValidatingMessage.SAVING_TARGET_AMOUNT_EMPTY_ERROR, response.getMessage());
+	}
 }

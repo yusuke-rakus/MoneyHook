502dba04a633d3cede448713c361aedf506fa16c
masatonara83 <masato.nara@rakus-partners.co.jp>
2023-06-27 18:09:01 +0900

    パスワード再設定画面テスト完了

--- MoneyLogAPI/src/test/java/com/example/controller/user/UserControllerResetPasswordPageTest.java
+++ MoneyLogAPI/src/test/java/com/example/controller/user/UserControllerResetPasswordPageTest.java
@@ -18,28 +18,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.common.SHA256;
 import com.example.common.Status;
-import com.example.common.message.ErrorMessage;
-import com.example.domain.User;
-import com.example.form.ForgotPasswordResetForm;
+import com.example.form.ResetPasswordPageForm;
 import com.example.mapper.UserMapper;
-import com.example.response.ForgotPasswordResetResponse;
+import com.example.response.ResetPasswordPageResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class)
-class UserControllerForgetPasswordResetTest {
+class UserControllerResetPasswordPageTest {
 	
-	final String SEND_URL = "/user/resetPasswordPage";
-	final String RESET_URL = "/user/forgotPasswordReset";
+	final String URL = "/user/resetPasswordPage";
 	final String PASSWORD = "password";
 	final String USER_ID = "a77a6e94-6aa2-47ea-87dd-129f580fb669";
 	final String EMAIL = "sample@sample.com";
@@ -60,19 +55,17 @@ class UserControllerForgetPasswordResetTest {
 	@Order(1)
 	@Test
 	@Transactional(readOnly = false)
-	void passwordResetSuccessTest() throws Exception {
+	void getPasswordResetPageTest() throws Exception {
 		/*準備*/
-		String newPass = "junittest";
-		ForgotPasswordResetForm form = new ForgotPasswordResetForm(); 
-		form.setEmail(EMAIL);
-		form.setPassword(newPass);
-		
 		String updateSql = "UPDATE user Set reset_password_param = ? WHERE user_id = ?";
 		String hashEmail = SHA256.getHashedValue(EMAIL);
 		jdbcTemplate.update(updateSql, hashEmail, USER_ID);
 		
+		ResetPasswordPageForm form = new ResetPasswordPageForm(); 
+		form.setParam(hashEmail);
+		
 		//パスワードを忘れた場合の再設定の実行
-		String result = mvc.perform(post(RESET_URL)
+		String result = mvc.perform(post(URL)
 				.content(mapper.writeValueAsString(form))
 				.contentType(MediaType.APPLICATION_JSON))
 				.andDo(print())
@@ -81,42 +74,29 @@ class UserControllerForgetPasswordResetTest {
 				.getResponse()
 				.getContentAsString(Charset.defaultCharset());
 		
-		//DBに登録されているかを確認
-		String sql = "SELECT * FROM user WHERE user_id = ?";
-		RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
-		User user = jdbcTemplate.queryForObject(sql, rowMapper, USER_ID);
-		
-		String hashPass = SHA256.getHashedPassword(newPass);
-		
+
 		//検証
-		ForgotPasswordResetResponse response = mapper.readValue(result, ForgotPasswordResetResponse.class);
+		ResetPasswordPageResponse response = mapper.readValue(result, ResetPasswordPageResponse.class);
 		assertEquals(Status.SUCCESS.getStatus(), response.getStatus());
-		assertNull(user.getResetPasswordParam());
-		assertEquals(user.getPassword(), hashPass);
+		assertEquals(EMAIL, response.getEmail());
 	}
 	
-	/*
-	 * DBエラーによるパスワード再設定がエラーになった際のテスト
-	 * */
 	@Order(2)
 	@Test
 	@Transactional(readOnly = false)
-	void passwordResetTErrorTest() throws Exception {
+	void getPasswordResetPageErrorTest() throws Exception {
 		/*準備*/
-		String newPass = "junittest";
-		ForgotPasswordResetForm form = new ForgotPasswordResetForm(); 
-		form.setEmail(EMAIL);
-		form.setPassword(newPass);
-		
 		String updateSql = "UPDATE user Set reset_password_param = ? WHERE user_id = ?";
 		String hashEmail = SHA256.getHashedValue(EMAIL);
 		jdbcTemplate.update(updateSql, hashEmail, USER_ID);
 		
-		//モック化
-		doThrow(new RuntimeException()).when(userMapper).resetPassword(any());
+		ResetPasswordPageForm form = new ResetPasswordPageForm(); 
+		form.setParam(hashEmail);
+		
+		doThrow(new RuntimeException()).when(userMapper).resetPasswordPage(any());
 		
 		//パスワードを忘れた場合の再設定の実行
-		String result = mvc.perform(post(RESET_URL)
+		String result = mvc.perform(post(URL)
 				.content(mapper.writeValueAsString(form))
 				.contentType(MediaType.APPLICATION_JSON))
 				.andDo(print())
@@ -125,18 +105,9 @@ class UserControllerForgetPasswordResetTest {
 				.getResponse()
 				.getContentAsString(Charset.defaultCharset());
 		
-		//DBに登録されているかを確認
-		String sql = "SELECT * FROM user WHERE user_id = ?";
-		RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
-		User user = jdbcTemplate.queryForObject(sql, rowMapper, USER_ID);
-		
-		String hashPass = SHA256.getHashedPassword(PASSWORD);
-		
+
 		//検証
-		ForgotPasswordResetResponse response = mapper.readValue(result, ForgotPasswordResetResponse.class);
+		ResetPasswordPageResponse response = mapper.readValue(result, ResetPasswordPageResponse.class);
 		assertEquals(Status.ERROR.getStatus(), response.getStatus());
-		assertEquals(ErrorMessage.FORGOT_RESET_PASSWORD_ERROR, response.getMessage());
-		assertNotNull(user.getResetPasswordParam());
-		assertEquals(user.getPassword(), hashPass);
 	}
 }

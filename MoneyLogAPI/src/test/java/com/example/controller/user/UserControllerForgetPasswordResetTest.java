2b6cfc721aa85895c5597511ffe4bca7b381dcdc
masatonara83 <masato.nara@rakus-partners.co.jp>
2023-06-27 17:17:09 +0900

    パスワード再設定テストを追加

--- MoneyLogAPI/src/test/java/com/example/controller/user/UserControllerForgetPasswordResetTest.java
+++ MoneyLogAPI/src/test/java/com/example/controller/user/UserControllerForgetPasswordResetTest.java
@@ -2,21 +2,17 @@ package com.example.controller.user;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.Charset;
 
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,20 +28,20 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.common.SHA256;
 import com.example.common.Status;
 import com.example.common.message.ErrorMessage;
-import com.example.common.message.SuccessMessage;
 import com.example.domain.User;
-import com.example.form.ForgotPasswordSendEmailForm;
+import com.example.form.ForgotPasswordResetForm;
 import com.example.mapper.UserMapper;
-import com.example.response.ForgotPasswordSendEmailResponse;
+import com.example.response.ForgotPasswordResetResponse;
 import com.example.service.SendMailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class)
-class UserControllerForgetPasswordSendEmailTest {
+class UserControllerForgetPasswordResetTest {
 	
-	final String URL = "/user/forgotPasswordSendEmail";
+	final String SEND_URL = "/user/forgotPasswordSendEmail";
+	final String RESET_URL = "/user/forgotPasswordReset";
 	final String PASSWORD = "password";
 	final String USER_ID = "a77a6e94-6aa2-47ea-87dd-129f580fb669";
 	final String EMAIL = "sample@sample.com";
@@ -69,16 +65,19 @@ class UserControllerForgetPasswordSendEmailTest {
 	@Order(1)
 	@Test
 	@Transactional(readOnly = false)
-	void forgetPasswordSendEmailSuccessTest() throws Exception {
+	void passwordResetSuccessTest() throws Exception {
 		/*準備*/
-		ForgotPasswordSendEmailForm form = new ForgotPasswordSendEmailForm(); 
+		String newPass = "junittest";
+		ForgotPasswordResetForm form = new ForgotPasswordResetForm(); 
 		form.setEmail(EMAIL);
+		form.setPassword(newPass);
 		
-		//モック化
-		doNothing().when(mockSendMailService).sendMail(any(), anyString(), anyString(), anyString());
+		String updateSql = "UPDATE user Set reset_password_param = ? WHERE user_id = ?";
+		String hashEmail = SHA256.getHashedValue(EMAIL);
+		jdbcTemplate.update(updateSql, hashEmail, USER_ID);
 		
-		//実行
-		String result = mvc.perform(post(URL)
+		//パスワードを忘れた場合の再設定の実行
+		String result = mvc.perform(post(RESET_URL)
 				.content(mapper.writeValueAsString(form))
 				.contentType(MediaType.APPLICATION_JSON))
 				.andDo(print())
@@ -92,67 +91,37 @@ class UserControllerForgetPasswordSendEmailTest {
 		RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
 		User user = jdbcTemplate.queryForObject(sql, rowMapper, USER_ID);
 		
-		String hashPass = SHA256.getHashedValue(EMAIL);
+		String hashPass = SHA256.getHashedPassword(newPass);
 		
 		//検証
-		verify(mockSendMailService, times(1)).sendMail(any(), anyString(), anyString(), anyString());
-		
-		ForgotPasswordSendEmailResponse response = mapper.readValue(result, ForgotPasswordSendEmailResponse.class);
+		ForgotPasswordResetResponse response = mapper.readValue(result, ForgotPasswordResetResponse.class);
 		assertEquals(Status.SUCCESS.getStatus(), response.getStatus());
-		assertEquals(SuccessMessage.FORGOT_PASSWORD_EMAIL_SUCCESS, response.getMessage());
-		assertNotNull(user.getResetPasswordParam());
-		assertEquals(user.getResetPasswordParam(), hashPass);
-	}
-	
-	@Order(2)
-	@Test
-	@Transactional(readOnly = false)
-	void forgetPasswordSendEmailErrorTest() throws Exception {
-		/*準備*/
-		String notEmail = "junittest01@example.com";
-		
-		ForgotPasswordSendEmailForm form = new ForgotPasswordSendEmailForm(); 
-		form.setEmail(notEmail);
-		
-		//モック化
-		doNothing().when(mockSendMailService).sendMail(any(), anyString(), anyString(), anyString());
-		
-		//実行
-		String result = mvc.perform(post(URL)
-				.content(mapper.writeValueAsString(form))
-				.contentType(MediaType.APPLICATION_JSON))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andReturn()
-				.getResponse()
-				.getContentAsString(Charset.defaultCharset());
-		
-		//検証
-		/* メール送信が呼ばれていないか */
-		verify(mockSendMailService, times(0)).sendMail(any(), anyString(), anyString(), anyString());
-		
-		ForgotPasswordSendEmailResponse response = mapper.readValue(result, ForgotPasswordSendEmailResponse.class);
-		assertEquals(Status.ERROR.getStatus(), response.getStatus());
-		assertEquals(ErrorMessage.EMAIL_NOT_EXIST_ERROR, response.getMessage());
+		assertNull(user.getResetPasswordParam());
+		assertEquals(user.getPassword(), hashPass);
 	}
 	
 	/*
-	 * DBエラー発生の際のテスト
+	 * DBエラーによるパスワード再設定がエラーになった際のテスト
 	 * */
-	@Order(3)
+	@Order(2)
 	@Test
 	@Transactional(readOnly = false)
-	void xdbErrorTest() throws Exception {
+	void passwordResetTErrorTest() throws Exception {
 		/*準備*/
-		ForgotPasswordSendEmailForm form = new ForgotPasswordSendEmailForm(); 
+		String newPass = "junittest";
+		ForgotPasswordResetForm form = new ForgotPasswordResetForm(); 
 		form.setEmail(EMAIL);
+		form.setPassword(newPass);
+		
+		String updateSql = "UPDATE user Set reset_password_param = ? WHERE user_id = ?";
+		String hashEmail = SHA256.getHashedValue(EMAIL);
+		jdbcTemplate.update(updateSql, hashEmail, USER_ID);
 		
 		//モック化
-		doThrow(new RuntimeException()).when(userMapper).setResetPasswordParam(any());
-		doNothing().when(mockSendMailService).sendMail(any(), anyString(), anyString(), anyString());
+		doThrow(new RuntimeException()).when(userMapper).resetPassword(any());
 		
-		//実行
-		String result = mvc.perform(post(URL)
+		//パスワードを忘れた場合の再設定の実行
+		String result = mvc.perform(post(RESET_URL)
 				.content(mapper.writeValueAsString(form))
 				.contentType(MediaType.APPLICATION_JSON))
 				.andDo(print())
@@ -161,20 +130,18 @@ class UserControllerForgetPasswordSendEmailTest {
 				.getResponse()
 				.getContentAsString(Charset.defaultCharset());
 		
-		//DBに登録されていないかを確認
+		//DBに登録されているかを確認
 		String sql = "SELECT * FROM user WHERE user_id = ?";
 		RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
 		User user = jdbcTemplate.queryForObject(sql, rowMapper, USER_ID);
 		
-		
+		String hashPass = SHA256.getHashedPassword(PASSWORD);
 		
 		//検証
-		/* メール送信が呼ばれていないか */
-		verify(mockSendMailService, times(0)).sendMail(any(), anyString(), anyString(), anyString());
-		
-		ForgotPasswordSendEmailResponse response = mapper.readValue(result, ForgotPasswordSendEmailResponse.class);
+		ForgotPasswordResetResponse response = mapper.readValue(result, ForgotPasswordResetResponse.class);
 		assertEquals(Status.ERROR.getStatus(), response.getStatus());
-		assertEquals(ErrorMessage.SYSTEM_ERROR, response.getMessage());
-		assertNull(user.getResetPasswordParam());
+		assertEquals(ErrorMessage.FORGOT_RESET_PASSWORD_ERROR, response.getMessage());
+		assertNotNull(user.getResetPasswordParam());
+		assertEquals(user.getPassword(), hashPass);
 	}
 }

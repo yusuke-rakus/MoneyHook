3e4a8ef1e00e3aa7b47d140ce59a28ea8929db73
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2023-06-28 21:38:55 +0900

    トークンMapperの修正

--- MoneyLogAPI/src/main/java/com/example/mapper/UserTokenMapper.java
+++ MoneyLogAPI/src/main/java/com/example/mapper/UserTokenMapper.java
@@ -6,9 +6,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserTokenMapper {
 	/** トークンを更新 */
-	public boolean updateToken(GoogleSignInForm form);
+	public void updateToken(GoogleSignInForm form);
 
 	/** トークンを登録 */
-	public boolean insertToken(GoogleSignInForm form);
+	public void insertToken(GoogleSignInForm form);
 
 }

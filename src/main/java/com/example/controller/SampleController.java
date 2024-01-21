0e4737cc567a1a7ab82f54bc3887c96c46750800
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2024-01-21 18:43:51 +0900

    fix

--- src/main/java/com/example/controller/SampleController.java
+++ src/main/java/com/example/controller/SampleController.java
@@ -1,17 +1,20 @@
 package com.example.controller;
 
+import com.example.response.CategoryResponse;
+import com.example.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-	//	@Autowired
-	//	private CategoryService categoryService;
+	@Autowired
+	private CategoryService categoryService;
 
 	@GetMapping("/")
 	public String getCategory() {
-		//		CategoryResponse response = categoryService.getCategoryList();
-		//		System.out.println(response.getCategoryList().get(0).getCategoryName());
+		CategoryResponse response = categoryService.getCategoryList();
+		System.out.println(response.getCategoryList().get(0).getCategoryName());
 		return "OKOKOKOK";
 	}
 }
